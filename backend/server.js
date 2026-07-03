1da54c02487ae4df7fbcf25d7f1dac9b0de61203
mina aithimmi <aithimmiamina2@gmail.com>
2026-07-03 04:51:40 +0200

    Création de l'API GET /api/services

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,21 @@
 require("dotenv").config();
-require("./config/db");
+
 const express = require("express");
 const cors = require("cors");
 
+require("./config/db");
+
+const serviceRoutes = require("./routes/serviceRoutes");
+
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Route de test
-app.get("/api/test", (req, res) => {
-  res.json({
-    success: true,
-    message: "Bienvenue sur l'API de réservation 🚀"
-  });
-});
-
-const serviceRoutes = require("./routes/serviceRoutes");
 app.use("/api/services", serviceRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+    console.log(`🚀 Serveur démarré sur http://localhost:${PORT}`);
 });
